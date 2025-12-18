884f55cfaf832b884e6714e8e05df74204d3dc53
dresswithpockets <dresswithpockets@pm.me>
2025-12-17 17:01:30 -0700

    add twofactor requests

---
+++ src/twofactor.rs
@@ -0,0 +1,39 @@
+use reqwest::Method;
+use serde::Deserialize;
+
+use crate::{
+    steamproto::{CTwoFactorTimeRequest, CTwoFactorTimeResponse},
+    transport::{EncodedProtobufBody, PrivateTransportRequest},
+};
+
+#[derive(Debug, Default)]
+pub struct QueryTimeRequest;
+
+impl QueryTimeRequest {
+    pub fn new() -> Self {
+        QueryTimeRequest::default()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct QueryTimeResponse {}
+
+impl<'a> From<QueryTimeRequest>
+    for PrivateTransportRequest<
+        'a,
+        EncodedProtobufBody<CTwoFactorTimeRequest>,
+        CTwoFactorTimeResponse,
+    >
+{
+    fn from(_value: QueryTimeRequest) -> Self {
+        let request = CTwoFactorTimeRequest::default();
+
+        PrivateTransportRequest::builder()
+            .method(Method::POST)
+            .path("/ITwoFactorService/QueryTime/v0001")
+            .body(request.into())
+            .build()
+    }
+}
+
+// TODO: do we need SteamTime() and AlignTime()?
