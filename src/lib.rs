a02dcf768180508246fcf50fc611a424e0c58028
dresswithpockets <dresswithpockets@pm.me>
2025-12-17 17:01:47 -0700

    add missing mods

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ pub mod econ;
 pub mod steamid;
 pub mod steamlang;
 pub mod tf2econ;
+pub mod tradeoffer;
 pub mod transport;
+pub mod twofactor;
 
 pub mod steamproto {
     include!(concat!(env!("OUT_DIR"), "/steamproto.rs"));
